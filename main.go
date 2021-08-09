bd3818dcfe74bcf13eafdb9002cffc8b1d5a109e
Dal Yoon <yeodal89@gmail.com>
2021-08-09 15:43:53 +0900

    #4.2 Our First Blockchain

--- main.go
+++ main.go
@@ -11,10 +11,42 @@ type block struct {
 	prevHash string
 }
 
+type blockchain struct {
+	blocks []block
+}
+
+func generateHash(data, prevHash string) string {
+	hash := sha256.Sum256([]byte(data + prevHash))
+	return fmt.Sprintf("%x", hash)
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+
+	return ""
+}
+
+func (b *blockchain) addBlocks(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	newBlock.hash = generateHash(newBlock.data, newBlock.prevHash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev Hash: %s\n", block.prevHash)
+	}
+}
+
 func main() {
-	genesisBlock := block{"Genisis Block", "", ""}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	hashAsHex := fmt.Sprintf("%x", hash)
-	genesisBlock.hash = hashAsHex
-	fmt.Println(genesisBlock)
+	chain := blockchain{}
+	chain.addBlocks("Genesis Block")
+	chain.addBlocks("Second Block")
+	chain.addBlocks("Third Block")
+
+	chain.listBlocks()
 }
