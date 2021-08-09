e294ca23ed45be12b31cf515fd1e1fedc7ed0733
Dal Yoon <yeodal89@gmail.com>
2021-08-09 15:02:39 +0900

    #4.1 Our First Block

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
 func main() {
+	genesisBlock := block{"Genisis Block", "", ""}
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hashAsHex := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hashAsHex
+	fmt.Println(genesisBlock)
 }
