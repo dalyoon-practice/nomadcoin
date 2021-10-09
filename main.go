5cba00040c83d13a4bacf0e4fc7774ed5731bf75
Dal Yoon <yeodal89@gmail.com>
2021-10-09 14:02:49 +0900

    REFACTOR

--- main.go
+++ main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"text/template"
-
-	"github.com/dalyoon-practice/nomadcoin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"This is Home", blockchain.GetBlockchain().AllBlocks()}
-
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+import "github.com/dalyoon-practice/nomadcoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Server listening on port http://localhost%s\n", port)
-	http.ListenAndServe(port, nil)
+	explorer.Start()
 }
